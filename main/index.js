572df46e448737f8ef8bea32bab19d921102b5d4
Sakthi7081 <Sakthivel434@gmail.com>
2020-04-23 14:45:37 +0530

    Commited JSON file on Thu Apr 23 2020 14:45:36 GMT+0530 (India Standard Time)

--- main/index.js
+++ main/index.js
@@ -1,11 +1,20 @@
 const fetch = require("node-fetch");
 const fs = require('fs');
+const cron = require("node-cron");
+const git = require('simple-git');
 
 const Repos = ['repo1','repo2','repo3'];
 
 const BitRepo = 'https://api.bitbucket.org/2.0/repositories/sakthivel434/';
 
 const File = '/src/master/package.json';
+
+const USER = 'sakthi7081';
+const PASS = 'sakthi@70';
+const REPO = 'git@github.com:sakthi7081/sample-repo.git';
+
+
+const remote = `https://${USER}:${PASS}@${REPO}`;
 
 let requests = Repos.map(repo => BitRepo +  repo + File);
 
@@ -15,15 +24,28 @@ const getRepo = async (repo) => {
     return json.Response;
   };
 
-  const getData = async () => {
-    return Promise.all(requests.map(item => getRepo(item)))
+  const getData = (fileName) => {
+    return Promise.all(requests.map(item => getRepo(item))).then(data => {
+      let response = {Transaction : true, Response : data};
+      fs.writeFile(fileName, JSON.stringify(response,null,2), () => { 
+        require('simple-git')()
+        .add('./*')
+        .commit('Commited JSON file on '+ (new Date()))
+        .addRemote('origin', remote)
+        .push(['-u', 'origin', 'master'], () => console.log('done'));               
+          // git.add('./RepoFile.json').commit('Commited JSON file on '+ (new Date()))
+          // .addRemote('origin', remote)
+          // .push(['-u', 'origin', 'master'], () => console.log('done'));
+    });
+  });
   }
 
-  getData().then(data => {
-      let response = {Transaction : true, Response : data};
-      fs.writeFile('RepoFile.json', JSON.stringify(response,null,2), () => {                
-        console.log('Repo data saved!');        
-    });
-});
+  getData('RepoFile.json');
 
+  // var task = cron.schedule("* * * * 6",getData('RepoFile.json'));
+
+  // task.start();
+
+  
+ 
 module.exports = getData;
--- main/main.test.js
+++ main/main.test.js
@@ -1,20 +1,19 @@
 const getData = require('./index');
-
-const fs = jest.genMockFromModule("fs");
-
+const fs = require('fs');
+jest.mock('fs');
 
 describe('Test the Repo write functionality',()=>{
     
-    it('Should write the file in the repo',async ()=>{
-        const logSpy = jest.spyOn(console, 'log');        
+    it('Should write the file in the repo',async ()=>{               
+        console.log = jest.fn();        
         fetch = jest.fn().mockResolvedValue({
             json: () => data
-        });    
-        jest.spyOn(fs, 'writeFile').mockImplementation((path, options, callback) => {
-                console.log('Repo data saved!');
-        });
-        await getData();  
-        expect(logSpy).toBeCalledWith('Repo data saved!');        
+        });          
+        jest.spyOn(fs, 'writeFile').mockImplementation((path, options, callback) => 
+            console.log('Repo data saved!')
+        )       
+        await getData('somefile');             
+        expect(console.log).toHaveBeenCalledWith('Repo data saved!');
     });
 
 });
